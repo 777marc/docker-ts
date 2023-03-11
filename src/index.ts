2fefad32ab30fbe6de96226e562958b42b2cb87f
Marc Mendoza <777marc@gmail.com>
2023-03-11 13:23:21 -0500

    init

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import express, {Request, Response} from "express";
+
+const app = express();
+
+app.get("/", (req: Request, res: Response) => {
+    return res.json({ status: 200, message: "all is well, Marcos!" });
+});
+
+app.get("/heyo", (req: Request, res: Response) => {
+    return res.json({ message: "heyo" });
+});
+
+app.listen(4000, () => console.log("listening on port 4000"));
