ba0c12ddec48c192cdf1d75c8a6bc20d26e188cb
Marc Mendoza <777marc@gmail.com>
2023-11-02 14:05:53 -0400

    add readme

---
+++ readme.md
@@ -0,0 +1,4 @@
+### instructions
+
+1. build : $ docker build -t myapp .
+2. run: $ docker run --publish 3000:3000 myapp
--- src/index.ts
+++ src/index.ts
@@ -10,4 +10,4 @@ app.get("/heyo", (req: Request, res: Response) => {
   return res.json({ message: "heyo!!!!" });
 });
 
-app.listen(4000, () => console.log("listening on port 4000"));
+app.listen(3000, () => console.log("listening on port 3000"));
